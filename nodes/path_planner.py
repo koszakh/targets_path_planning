71c8e1e88b55db4c44330ed0175d3342d9ba0b75
Konstantin Zakharov <kon7666007@yandex.ru>
2021-08-13 17:46:00 +0300

    Heightmap class fixed

--- nodes/path_planner.py
+++ nodes/path_planner.py
@@ -12,22 +12,25 @@ import path_planning.Constants as const
 import gazebo_communicator.GazeboCommunicator as gc
 import gazebo_communicator.GazeboConstants as gc_const
 from path_planning.ORCA import ORCAsolver
+import random
 
 def group_path_planning():
 	paths_pub = rospy.Publisher('all_paths_data', AllPaths, queue_size=10)
-	hm = Heightmap(const.HEIGHTMAP_PATH, const.HEIGHTMAP_SDF_PATH)
-	hmap, l_scale, w_scale, x_step, y_step, grid_step, step_count = hm.prepare_heightmap(const.MIN_COL, const.MAX_COL, const.MIN_ROW, const.MAX_ROW)
+	hm = Heightmap(const.HEIGHTMAP_SDF_PATH)
+	hmap, l_scale, w_scale, x_step, y_step, grid_step, step_count = \
+		hm.prepare_heightmap(const.MIN_COL, const.MAX_COL, const.MIN_ROW, const.MAX_ROW)
+		
 	map_handler = pp.PathPlanner(hmap, l_scale, w_scale, grid_step, x_step, y_step, step_count)
-	map_handler.cells_maker()
-	cells = map_handler.cells
+
 	map_handler.gridmap_preparing()
+	cells = map_handler.cells
+	print('Vertices count: ' + str(len(map_handler.heightmap)))
+
 	orca = ORCAsolver(map_handler.heightmap, cells, x_step, y_step, l_scale, w_scale)
-	smoothed_paths = {}
 	robot_names = []
-	f = open('/root/catkin_ws/src/targets_path_planning/nodes/orca_data.py', 'w+')
-	f.write('from path_planning.Point import Point\n')
-	f.close()
+
 	for name in gc_const.ROBOT_NAMES:
+
 		robot_names.append(name)
 		robot_pos, orient = map_handler.get_random_start_pos()
 		gc.spawn_target(name, robot_pos, orient)
