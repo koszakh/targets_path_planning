b218208fa7db0f49724585045fad873cc9a022d0
Konstantin Zakharov <kon7666007@yandex.ru>
2021-08-17 17:40:26 +0300

    Add files via upload

--- src/gazebo_communicator/GazeboCommunicator.py
+++ src/gazebo_communicator/GazeboCommunicator.py
@@ -16,6 +16,7 @@ from gazebo_msgs.srv import SetModelState, GetModelState, GetModelProperties, Sp
 import vector3d
 import threading as thr
 from scipy.spatial.transform import Rotation
+from sensor_msgs.msg import NavSatFix
 
 # The class of the control object of the ground target in the Gazebo simulation environment
 
@@ -29,7 +30,9 @@ from scipy.spatial.transform import Rotation
 # finished_planning: a flag whose true value means that the robot has reached the end point in the ORCA simulation
 # last_goal: previous point of the robot route
 class Robot(thr.Thread):
+	
 	def __init__(self, robot_name):
+	
 		thr.Thread.__init__(self)
 		print('\n!!! ' + robot_name + ' initialised !!!\n')
 		subtopic_name = '/sim_' + robot_name
@@ -38,6 +41,7 @@ class Robot(thr.Thread):
 		self.waypoint_sub = rospy.Subscriber(subtopic_name + '/waypoint', Point, self.waypoint_callback)
 		self.waypoints_pub = rospy.Publisher(subtopic_name + '/waypoints_array', Path, queue_size=10)
 		self.waypoints_sub = rospy.Subscriber(subtopic_name + '/waypoints_array', Path, self.set_path)
+		self.gps_listener = rospy.Subscriber(subtopic_name + '/fix', NavSatFix, self.gps_callback)
 		self.pid_delay = rospy.Duration(0, const.PID_NSEC_DELAY)
 		msg = Twist()
 		self.vel_publisher.publish(msg)
@@ -46,13 +50,24 @@ class Robot(thr.Thread):
 		self.ms = const.MOVEMENT_SPEED
 		self.dir_point = robot_name + const.DIR_POINT_SUFFIX
 		self.path = []
+		self.latitude = None
+		self.longitude = None
+		
+	def gps_callback(self, msg_data):
+	
+		self.latitude = msg_data.latitude
+		self.longitude = msg_data.longitude
 
+	def get_gps_coords(self):
+	
+		return '(' + str(self.longitude) + ', ' + str(self.latitude) + ')'
 
 # Getting the position of the robot in 3D space in the Gazebo environment
 
 # Output
 # robot_pose: coordinates of the robot in 3D space
 	def get_robot_position(self):
+	
 		robot_pose = get_model_position(self.name)
 		return robot_pose
 
@@ -61,6 +76,7 @@ class Robot(thr.Thread):
 # Output
 # dir_vector: robot direction vector
 	def get_robot_orientation_vector(self):
+	
 		robot_pos = self.get_robot_position()
 		dir_point_pos = get_link_position(self.dir_point)
 		dir_vector = robot_pos.get_dir_vector_between_points(dir_point_pos)
@@ -83,13 +99,16 @@ class Robot(thr.Thread):
 # Input
 # goal: target point
 	def move_with_PID(self, goal):
+	
 		#print(self.name + ' moving to the ' + str(goal) + ' point.')
 		#self.turn_to_point(goal)
 		old_error = 0
 		error_sum = 0
 		real_error_sum = 0
 		robot_pos = self.get_robot_position()
+		
 		while robot_pos.get_distance_to(goal) > const.DISTANCE_ERROR:
+		
 			robot_pos = self.get_robot_position()
 			error = self.get_angle_difference(goal)
 			error_sum += error
@@ -106,6 +125,7 @@ class Robot(thr.Thread):
 			#print(self.name + ' error: ' + str(error) + ' | u: ' + str(u))
 			self.movement(self.ms, u)
 			rospy.sleep(self.pid_delay)
+		
 		#self.stop()
 
 
@@ -113,18 +133,26 @@ class Robot(thr.Thread):
 # Input
 # goal: target point
 	def turn_to_point(self, goal):
+	
 		angle_difference = self.get_angle_difference(goal)
+		
 		while fabs(angle_difference) > const.ANGLE_ERROR:
+		
 			angle_difference = self.get_angle_difference(goal)
-			#print(str(self.name) + ' angle difference: ' + str(angle_difference))
+		
 			if angle_difference > 0:
+		
 				self.movement(0, const.ROTATION_SPEED)
+		
 			else:
+		
 				self.movement(0, -const.ROTATION_SPEED)
+		
 		self.stop()
 
 # Stopping the robot
 	def stop(self):
+	
 		msg = Twist()
 		msg.linear.x = 0
 		msg.angular.z = 0
@@ -135,20 +163,24 @@ class Robot(thr.Thread):
 # move_speed: linear velocity (m/s)
 # rotation_speed: 
 	def movement(self, move_speed, rotation_speed):
+	
 		msg = Twist()
 		msg.linear.x = move_speed
 		msg.angular.z = rotation_speed
 		self.vel_publisher.publish(msg)
 
 	def waypoint_callback(self, msg):
+	
 		point = convert_to_point(msg.point)
 		self.path.append(point)
 
 	def waypoint_publisher(self, point):
+	
 		msg = prepare_point_msg(point)
 		self.waypoint_pub.publish(msg)
 
 	def waypoints_publisher(self, path):
+	
 		msg = prepare_path_msg(self.name, path)
 		self.waypoints_pub.publish(msg)
 
@@ -156,6 +188,7 @@ class Robot(thr.Thread):
 # Input
 # path: list of path points
 	def set_path(self, msg_data):
+	
 		path = convert_to_path(msg_data.path)
 		self.path = path
 		print(self.name + ' path vertices count: ' + str(len(path)))
@@ -164,14 +197,34 @@ class Robot(thr.Thread):
 # The movement of the robot along a given final route
 # Start of thread
 	def run(self):
+		
+		start_coords = self.get_gps_coords()
+		f = open(const.MAP_COORDS_PATH, 'a+')
+		f.write('Target ' + self.name[4:] + ' start coords: ' + str(start_coords) + '\n')
+		f.close()
+		#print(self.name + ' start GPS coordinates: ' + self.get_gps_coords())
 		print(self.name + ' path length: ' + str(get_path_length(self.path)))
+		
 		if len(self.path) > 0:
+		
 			for state in self.path:
+		
 				dist = state.get_distance_to(self.get_robot_position())
+		
 				if dist > const.DISTANCE_ERROR * 2:
+		
 					self.move_with_PID(state)
+		
 			self.stop()
+			
+			end_coords = self.get_gps_coords()
+			f = open(const.MAP_COORDS_PATH, 'a+')
+			f.write('Target ' + self.name[4:] + ' end coords: ' + str(end_coords) + '\n')
+			f.close()
+			
+			print(self.name + ' end GPS coordinates: ' + self.get_gps_coords())
 			print('The robot ' + str(self.name) + ' has finished!')
+		
 		else:
 			print('Path is empty!')
 
@@ -199,14 +252,23 @@ def make_pose_msg(state, orient):
 # model_directory: the location on the device of the folder with the object model used when rendering the path
 # obj_name: the name used when rendering the waypoint
 def visualise_path(path, model_directory, obj_name):
+	
 	if path:
+	
 		i = 0
+	
 		for state in path:
+	
 			i += 1
+	
 			if state.id:
+	
 				vertice_name = obj_name + str(i) + '_' + str(state.id)
+	
 			else:
+	
 				vertice_name = obj_name + str(i)
+	
 			spawn_sdf_model(state, model_directory, vertice_name)
 
 # Getting data about a property of the world in the Gazebo environment
@@ -219,12 +281,17 @@ def visualise_path(path, model_directory, obj_name):
 #  bool success
 #  string status_message
 def get_world_properties():
+	
 	rospy.wait_for_service('/gazebo/get_world_properties/')
+	
 	try:
+	
 		get_world_prop = rospy.ServiceProxy('/gazebo/get_world_properties', GetWorldProperties)
 		world_properties = get_world_prop()
 		return world_properties
+	
 	except:
+	
 		print "Service call failed: %s" % e
 		return None
 
@@ -245,12 +312,17 @@ def get_world_properties():
 #  bool success
 #  string status_message
 def get_model_properties(model_name):
+	
 	rospy.wait_for_service('/gazebo/get_model_properties/')
+	
 	try:
+	
 		get_model_prop = rospy.ServiceProxy('/gazebo/get_model_properties', GetModelProperties)
 		model_properties = get_model_prop(model_name)
 		return model_properties
+	
 	except rospy.ServiceException, e:
+	
 		print "Service call failed: %s" % e
 		return None
 
@@ -262,12 +334,15 @@ def get_model_properties(model_name):
 # pose: position of an object in 3D space
 def get_model_position(model_name):
 	object_state = get_model_state(model_name)
+	
 	if object_state:
+	
 		x = object_state.pose.position.x
 		y = object_state.pose.position.y
 		z = object_state.pose.position.z
 		pose = PointGeom.Point(x, y, z)
 		return pose
+	
 	else:
 		return None
 
@@ -282,7 +357,9 @@ def get_model_position(model_name):
 #  string status_message 
 def get_link_position(link_name):
 	rospy.wait_for_service('/gazebo/get_link_state')
+	
 	try:
+	
 		get_link_state = rospy.ServiceProxy('/gazebo/get_link_state', GetLinkState)
 		link_state = get_link_state(link_name, 'world')
 		pos = link_state.link_state.pose.position
@@ -291,11 +368,14 @@ def get_link_position(link_name):
 		z = pos.z
 		state = PointGeom.Point(x, y, z)
 		return state
+	
 	except rospy.ServiceException, e:
+	
 		print "Service call failed: %s" % e
 		return None
 
 def get_robot_orientation_vector(robot_name):
+	
 		robot_pos = get_model_position(robot_name)
 		dir_point_pos = get_link_position(robot_name + const.DIR_POINT_SUFFIX)
 		dir_vector = robot_pos.get_dir_vector_between_points(dir_point_pos)
@@ -313,11 +393,15 @@ def get_robot_orientation_vector(robot_name):
 #  string status_message
 def get_model_state(model_name):
 	rospy.wait_for_service('/gazebo/get_model_state')
+	
 	try:
+	
 		get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
 		model_coordinates = get_state(model_name, 'world')
 		return model_coordinates
+	
 	except rospy.ServiceException, e:
+	
 		print "Service call failed: %s" % e
 		return None
 		
@@ -342,14 +426,18 @@ def delete_model(model_name):
 # state: the position at which the object will be spawned
 def spawn_sdf_model(state, model_directory, model_name):
 	rospy.wait_for_service('/gazebo/spawn_sdf_model')
+	
 	try:
+	
 		f = open(model_directory)
 		point_xml = f.read()
 		f.close()
 		pose = make_pose_msg(state, None)
 		spawn_model = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
 		resp = spawn_model(model_name, point_xml, '', pose, 'world')
+	
 	except rospy.ServiceException, e:
+	
 		print "Service call failed: %s" % e
 
 def spawn_target(model_name, state, orient):
@@ -364,11 +452,15 @@ def spawn_urdf_model(model_name, model_directory, state, orient):
 	z = orient[2]
 	w = orient[3]
 	rospy.wait_for_service('/gazebo/spawn_urdf_model')
+	
 	try:
+	
 		spawn_model_client = rospy.ServiceProxy('/gazebo/spawn_urdf_model', SpawnModel)
 		spawn_model_client(model_name, open(model_directory, 'r').read(),
 					"/sim_" + model_name, Pose(position=Point(state.x, state.y, state.z), orientation=Quaternion(x, y, z, w)), "world")
+	
 	except rospy.ServiceException, e:
+	
 		print "Service call failed: %s" % e
 
 # Setting the object of the required position and orientation in space
@@ -376,6 +468,7 @@ def spawn_urdf_model(model_name, model_directory, state, orient):
 # pose: position of an object in 3D space
 # orient: orientation of an object in 3D space
 def set_model_state(model_name, pose, orient):
+	
 	state_msg = ModelState()
 	state_msg.model_name = model_name
 	state_msg.pose.position.x = pose.x
@@ -386,10 +479,14 @@ def set_model_state(model_name, pose, orient):
 	state_msg.pose.orientation.z = orient[2]
 	state_msg.pose.orientation.w = orient[3]
 	rospy.wait_for_service('/gazebo/set_model_state')
+	
 	try:
+	
 		set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 		resp = set_state(state_msg)
+	
 	except:
+	
 		print "Service call failed: %s" % e
 
 # Getting a three-dimensional vector directed from point p1 to point p2
@@ -400,15 +497,14 @@ def set_model_state(model_name, pose, orient):
 # Output
 # dir_vector: vector between given points
 def get_orientation_vector(p1, p2):
+	
 	det_x = p2.x - p1.x
 	det_y = p2.y - p1.y
 	dir_vector = PointGeom.Vector2d(det_x, det_y)
-	#z = p2.z - p1.z
-	#dir_vector = vector3d.vector.Vector(x, y, z).normalize()
 	return dir_vector
 
 def get_3d_orientation_vector(p1, p2):
-	print('p1: ' + str(p1) + ' | p2: ' + str(p2))
+	
 	x = p2.x - p1.x
 	y = p2.y - p1.y
 	z = p2.z - p1.z
@@ -423,18 +519,24 @@ def get_3d_orientation_vector(p1, p2):
 # Output
 # angle: angle between vectors in degrees
 def get_3d_angle(p1, p2, p3):
+	
 	v1 = get_3d_orientation_vector(p2, p1)
 	v2 = get_3d_orientation_vector(p2, p3)
 	z_axis = vector3d.vector.Vector(0, 0, 1)
 	angle = vector3d.vector.angle(v1, v2)
+	
 	if not angle == 0:
+	
 		i = v1.y * v2.z - v1.z * v2.y
 		j = v1.x * v2.z - v1.z * v2.x
 		k = v1.x * v2.y - v1.y * v2.x
 		v3 = vector3d.vector.Vector(i, j, k).normalize()
 		z_angle = vector3d.vector.angle(v3, z_axis)
+	
 		if z_angle > 90:
+	
 			angle = - angle
+	
 	return angle
 
 # Convert 3D vector to 2D
@@ -444,6 +546,7 @@ def get_3d_angle(p1, p2, p3):
 # Output
 # vect2D: 2D vector representation
 def convert_3d_to_2d_vect(vector_3d):
+	
 	vect2d = PointGeom.Vector2d(vector_3d.x, vector_3d.y)
 	return vect2d
 
@@ -454,6 +557,7 @@ def convert_3d_to_2d_vect(vector_3d):
 # Output
 # point: Point object
 def convert_to_point(msg):
+	
 	x = msg.x
 	y = msg.y
 	z = msg.z
@@ -463,31 +567,41 @@ def convert_to_point(msg):
 # Output
 # max_curvature: path curvature
 def get_path_curvature(path):
+	
 	max_curvature = 0
+	
 	for i in range(0, len(path) - 2):
+	
 		p1 = path[i]
 		p2 = path[i + 1]
 		p3 = path[i + 2]
 		v1 = p1.get_dir_vector_between_points(p2)
 		v2 = p2.get_dir_vector_between_points(p3)
 		angle_difference = fabs(v1.get_angle_between_vectors(v2))
+	
 		if angle_difference > max_curvature:
+	
 			max_curvature = angle_difference
+	
 	return max_curvature
 
 def prepare_path_msg(name, path):
 	msg = Path()
 	msg.path = []
 	msg.robot_name = name
+	
 	for state in path:
+	
 		point = Point()
 		point.x = state.x
 		point.y = state.y
 		point.z = state.z
 		msg.path.append(point)
+	
 	return msg
 
 def prepare_point_msg(p):
+	
 	msg = Point()
 	msg.x = p.x
 	msg.y = p.y
@@ -495,13 +609,17 @@ def prepare_point_msg(p):
 	return msg
 
 def convert_to_path(msg):
+	
 	path = []
+	
 	for state in msg:
+	
 		x = state.x
 		y = state.y
 		z = state.z
 		p = PointGeom.Point(x, y, z)
 		path.append(p)
+	
 	return path
 	
 # Calculating path length
@@ -511,6 +629,7 @@ def convert_to_path(msg):
 # Output
 # path_len: path length
 def get_path_length(path):
+
 	path_len = 0
 
 	for i in range(0, len(path) - 1):
--- src/gazebo_communicator/GazeboConstants.py
+++ src/gazebo_communicator/GazeboConstants.py
@@ -1,8 +1,10 @@
-ROBOT_NAMES = ['p3at1', 'p3at2', 'p3at3']#, 'p3at4', 'p3at5']
-ROBOT_MODEL_PATH = "/root/catkin_ws/src/targets_path_planning/urdf/pioneer3at_"
+ROBOTS_COUNT = 5
+ROOT_PATH = '/root'
+ROBOT_MODEL_PATH = ROOT_PATH + '//catkin_ws/src/targets_path_planning/urdf/pioneer3at_'
 DIR_POINT_SUFFIX = '::dir_point'
-VERTICE_PATH = '/root/.gazebo/models/vertice/model.sdf'
-RED_VERTICE_PATH = '/root/.gazebo/models/red_vertice/model.sdf'
+VERTICE_PATH = ROOT_PATH + '/.gazebo/models/vertice/model.sdf'
+RED_VERTICE_PATH = ROOT_PATH + '/.gazebo/models/red_vertice/model.sdf'
+BIG_RED_VERTICE_PATH = ROOT_PATH + '/.gazebo/models/big_red_vertice/model.sdf'
 GREEN_VERTICE_PATH = '/root/.gazebo/models/green_vertice/model.sdf'
 BLUE_VERTICE_PATH = '/root/.gazebo/models/blue_vertice/model.sdf'
 PATH_COLORS = [RED_VERTICE_PATH, GREEN_VERTICE_PATH, BLUE_VERTICE_PATH, RED_VERTICE_PATH, GREEN_VERTICE_PATH]
@@ -14,15 +16,17 @@ DISTANCE_ERROR = 0.1
 PID_NSEC_DELAY = 200000000
 KP = 0.0025#0.005
 KI = 0.025#0.0375
-KD = 0.005#0.01
+KD = 0.0075#0.01
 I_MIN = -10
 I_MAX = 10
 SPAWN_HEIGHT_OFFSET = 0.05
 
 ORCA_TIME_STEP = 0.2
-ORCA_NEIGHBOR_DIST = 1
-ORCA_MAX_NEIGHBORS = len(ROBOT_NAMES)
+ORCA_NEIGHBOR_DIST = 2
+ORCA_MAX_NEIGHBORS = ROBOTS_COUNT
 ORCA_TIME_HORIZON = 1
 ORCA_TIME_HORIZON_OBST = 0.4
-ORCA_RADIUS = 0.5
+ORCA_RADIUS = 0.6
+
+MAP_COORDS_PATH = '/root/catkin_ws/src/targets_path_planning/map_logs/map_coords_1_1.txt'
 
